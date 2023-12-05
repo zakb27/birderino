d17a79bb750bc64082a7eae5c3b221e60922f32b
Zak Brook <46377498+zakb27@users.noreply.github.com>
2023-12-05 01:06:46 +0000

    reviewedtickets

--- src/pages/Flagged.js
+++ src/pages/Flagged.js
@@ -1,17 +1,50 @@
-import React from "react";
+import React,{useState} from "react";
 
 import AdminNav from "../components/AdminNav"
 import { useDispatch, useSelector } from 'react-redux';
+import {FaAngleDown, FaAngleUp} from "react-icons/fa";
 
-const Dash = () =>{
+
+const Reviewed = () =>{
+    
+}
+
+const Flagged = () =>{
+    const [flaggedExpanded,changeFlagged] = useState(false)
+    const [checkedExpanded,changeChecked] = useState(false)
+    const handleFlagged = () =>{
+
+        changeFlagged(!flaggedExpanded)
+    }
+    const handleChecked = () =>{
+
+        changeChecked(!checkedExpanded)
+    }
+
     return(
         <>
             <AdminNav />
             <div className={'flagPage background'}>
                 <h1>Flagged tickets</h1>
                 <div className="items_container">
-                    <div className="flagged"></div>
-                    <div className="fun"></div>
+                    <div className="flagged_container">
+                        {!flaggedExpanded?
+                            <button onClick={e=>{handleFlagged()}} > <FaAngleDown /></button>
+                            :
+                            <button onClick={e=>{handleFlagged()}} > <FaAngleUp /></button>
+                        }
+                        <h2>View flagged tickets</h2>
+                    </div>
+
+                    <div className="checked_container">
+                        {!checkedExpanded?
+                            <button onClick={e=>{handleChecked()}} > <FaAngleDown /></button>
+                            :
+                            <button onClick={e=>{handleChecked()}} > <FaAngleUp /></button>
+                        }
+                        <h2>View reviewed tickets</h2>
+
+                    </div>
                 </div>
             </div>
         </>
@@ -20,4 +53,4 @@ const Dash = () =>{
 }
 
 
-export default Dash;
+export default Flagged;
---
+++ src/tools/Reviewed.js
@@ -0,0 +1,10 @@
+export const Reviewed = [
+    {
+        email: 'test@email.com',
+        flag: 'The European robin (Erithacus rubecula) is a common garden bird in the UK. Known for its red breast and inquisitive nature, robins are often seen near humans. They have a diet that includes insects, worms, and berries. In folklore, robins are associated with Christmas and are considered symbols of good luck.',
+    },
+    {
+        email: 'test@email2.com',
+        flag: 'The European robin (Erithacus rubecula) is a common garden bird in the UK. Known for its red breast and inquisitive nature, robins are often seen near humans. They have a diet that includes insects, worms, and berries. In folklore, robins are associated with Christmas and are considered symbols of good luck.',
+    },
+];
